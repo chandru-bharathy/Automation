a97f57a7f98644e487039d0b6f6fd4440ed1bbe0
chandru-bharathy <92341379+chandru-bharathy@users.noreply.github.com>
2024-02-01 11:17:54 -0700

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -21,7 +21,7 @@ public class BrowserTest {
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
+		System.out.println("result: " + text);
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
 		driver.close();
 	
